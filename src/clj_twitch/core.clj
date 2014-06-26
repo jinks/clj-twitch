1f4d3fe5d842b6bd7e133e5a4a4892031b217246
Corinne Schrickel <corinne.schrickel@gmail.com>
2014-06-26 04:45:56 +0200

    replaced clj-http with http-kit, switched to v3 API

--- src/clj_twitch/core.clj
+++ src/clj_twitch/core.clj
@@ -1,18 +1,16 @@
 (ns clj-twitch.core
-  (:require [clj-http.client :as client]))
+  (:require [org.httpkit.client :as client]
+            [cheshire.core :as json]))
 
 (def base-url "https://api.twitch.tv/kraken")
 
 (defn fetch-twitch
   "Fetch JSON from an API URL and return as clojure map."
   [url]
-  (let [data (try (client/get url {:as :json
+  (let [data (try @(client/get url {
                                    ;setting a sensible timeout as Twitch is "twitchy"
-                                   :socket-timeout 800
-                                   :conn-timeout 800
-                                   ;v2 API only for now
-                                   :headers {"Accept" "application/vnd.twitchtv.v2+json"}
-                                   :retry-handler (fn [ex try-count http-context]
-                                               (if (> try-count 6) false true))})
+                                   :timeout 800
+                                   ;v3 API for more info
+                                   :headers {"Accept" "application/vnd.twitchtv.v3+json"}})
                (catch Exception e nil))]
-    (:body data)))
+    (json/parse-string (:body data) true)))
