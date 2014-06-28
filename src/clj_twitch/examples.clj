343fe041249ee803c205c00d54d5ad97417f5fa6
Corinne Schrickel <corinne.schrickel@gmail.com>
2014-06-28 02:42:39 +0200

    code cleanup

--- src/clj_twitch/examples.clj
+++ src/clj_twitch/examples.clj
@@ -10,31 +10,31 @@
 (defn summary
   [channel]
   "Returns a small summary of a given channel (name, played game, status, number of viewers and followers)."
-  (def cname (channel/pretty channel))
-  (def cgame (channel/game channel))
-  (def cstatus (channel/status channel))
-  (def vcount (stream/viewercount channel))
-  (def fcount (channel/followercount channel))
+  (let [cname (channel/pretty channel)
+        cgame (channel/game channel)
+        cstatus (channel/status channel)
+        vcount (stream/viewercount channel)
+        fcount (channel/followercount channel)]
   (str cname " playing " cgame " (" cstatus ") - " vcount " viewer"
-       (if (> vcount 1) "s") ", " fcount " follower" (if (> fcount 1) "s")))
+       (if (> vcount 1) "s") ", " fcount " follower" (if (> fcount 1) "s"))))
 
 (defn lastvictim
   [channel]
   "Returns the last user to follow a given channel and the time that has elapsed since then."
-  (def standard-formatter (format/formatters :date-time-no-ms))
-  (def lfollow (channel/followpretty (channel/lastfollow channel)))
-  (def elapsed (time/in-minutes
+  (let [standard-formatter (format/formatters :date-time-no-ms)
+        lfollow (channel/followpretty (channel/lastfollow channel))
+        elapsed (time/in-minutes
                  (time/interval
                   (format/parse standard-formatter
                    (channel/followdate (channel/lastfollow channel)))
-                  (time/now))))
-  (def cname (channel/pretty channel))
-  (letfn [(in-days-hours
-        [minutes]
-           (let [days (quot (quot minutes 60) 24)
-                 hours (mod (quot minutes 60) 24)]
-             (str (if (> minutes 1439) (str days " day" (if (> days 1) "s") " "))
-                  (if (> minutes 59) (str hours " hour" (if (> hours 1) "s") " "))
-             (mod minutes 60) " minute" (if (> minutes 1) "s")))
-        )]
-  (str lfollow " followed " cname " " (in-days-hours elapsed) " ago!")))
+                  (time/now)))
+        cname (channel/pretty channel)]
+    (letfn [(in-days-hours
+             [minutes]
+             (let [days (quot (quot minutes 60) 24)
+                   hours (mod (quot minutes 60) 24)]
+               (str (if (> minutes 1439) (str days " day" (if (> days 1) "s") " "))
+                    (if (> minutes 59) (str hours " hour" (if (> hours 1) "s") " "))
+                    (mod minutes 60) " minute" (if (> minutes 1) "s")))
+             )]
+      (str lfollow " followed " cname " " (in-days-hours elapsed) " ago!"))))
