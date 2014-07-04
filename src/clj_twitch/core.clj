a343f7b64e80211ed655ba705be6e2b6ec68461b
mondsucht <corinne.schrickel@gmail.com>
2014-07-04 17:23:08 +0200

    removed unused and unnecessary headers, adjusted timeout

--- src/clj_twitch/core.clj
+++ src/clj_twitch/core.clj
@@ -2,19 +2,15 @@
   (:require [org.httpkit.client :as client]
             [cheshire.core :as json]))
 
-(def base-url "https://api.twitch.tv/kraken")   ;ideally this will not change ever
-(def client-id nil)                             ;set to avoid rate limits
+(def base-url "https://api.twitch.tv/kraken")
 
 (defn fetch-twitch
   "Fetch JSON from an API URL and return as clojure map."
   [url]
   (let [data (try @(client/get url {
                                    ;setting a sensible timeout as Twitch is "twitchy"
-                                   :timeout 800
+                                   :timeout 1000
                                    ;v3 API for more info
-                                   :headers {"Accept" "application/vnd.twitchtv.v3+json"
-                                             ;don't give me old data kthxbye
-                                             "Cache-Control" "no-cache"
-                                             "Client-ID" (if-not (empty? client-id) (str client-id) "")}})
+                                   :headers {"Accept" "application/vnd.twitchtv.v3+json"}})
                (catch Exception e nil))]
     (json/parse-string (:body data) true)))
